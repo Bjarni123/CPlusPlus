8b92c346b736ee265ae80cfa8dbcb380a4617769
Bjarni123 <Bjarni04@gmail.com>
2023-02-21 10:15:30 +0000

    lok timans

--- lokaverkefni/bunadur.cpp
+++ lokaverkefni/bunadur.cpp
@@ -24,21 +24,21 @@ void Bunadur::setNR(int nr) {
 }
 
 int Bunadur::getStadsetning() {
-    return int(std::to_string(this->stadsetning[0]) + 
-    std::to_string(this->stadsetning[1]) + 
-    std::to_string(this->stadsetning[2]));
+    return this->stadsetning[0] * 1000 + this->stadsetning[1] * 100 + this->stadsetning[2];
 }
 
 void Bunadur::setStadsetning(int stadsetning) {
-
+    this->stadsetning[0] = stadsetning / 1000;
+    this->stadsetning[1] = (stadsetning - this->stadsetning[0] * 1000) / 100;
+    this->stadsetning[2] = stadsetning % 100;
 }
 
 int Bunadur::getVerd() {
-
+    return this->verd;
 }
 
 void Bunadur::setVerd(int verd) {
-
+    this->verd = verd;
 }
 
 void Bunadur::prenta() {
