ee70b170c007f126ac469bba215166d82cfa09e0
Bjarni123 <Bjarni04@gmail.com>
2023-02-22 15:11:01 +0000

    gerdi g&s fyrir stads. uppf. það og prenta i husi

--- lokaverkefni/main.cpp
+++ lokaverkefni/main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <cstdlib> // <stdlib.h> fyrir mac
+#include <ctime>
+#include <sstream>
 
+#include <fstream> // filestream
+
+#include "husgogn.h"
+#include "rlutil.h"
 
 using namespace std;
+using namespace rlutil;
 
 int main() {
+    Husgogn hg;
+    string inntak, skipun, tegund, nafn, eigandi;
+    int aldur, einkunn;
 
+    saveDefaultColor();
+
+    do {
+        
+    } while (skipun != "haetta");
+
+    resetColor();
     return 0;
 }
